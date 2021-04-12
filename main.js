581853d62fd4e226a3aafcb2f237d02b05858dbb
artiwok <artemolga@ukr.net>
2021-04-12 21:29:48 +0300

    some changes

--- main.js
+++ main.js
@@ -18,69 +18,69 @@ const kitana = {
   }
 };
 
-function createPlayer1() {
-  const $player1 = document.createElement('div');
-  $player1.classList.add('player1');
+function createPlayer(className, name, life, obj) {
+  $player = document.createElement('div');
+  $player.classList.add(`${className}`);
 
   const $progressbar = document.createElement('div');
   $progressbar.classList.add('progressbar');
-  $player1.appendChild($progressbar);
+  $player.appendChild($progressbar);
 
   const $character = document.createElement('div');
   $character.classList.add('character');
-  $player1.appendChild($character);
+  $player.appendChild($character);
 
   const $life = document.createElement('div');
   $life.classList.add('life');
-  $life.style.width = '100%';
+  $life.style.width = `${life}%`;
   // $life.innerText = life;
   $progressbar.appendChild($life);
 
   const $name = document.createElement('div');
   $name.classList.add('name');
-  $name.innerText = 'Scorpion';
+  $name.innerText = `${name}`;
   $progressbar.appendChild($name);
 
   const $img = document.createElement('img');
   $img.classList.add('img');
-  $img.src = 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif';
+  $img.src = obj.img;
   $character.appendChild($img);
 
   const $arenas = document.querySelector('.arenas');
-  $arenas.appendChild($player1);
+  $arenas.appendChild($player);
 };
 
-function createPlayer2() {
-  const $player2 = document.createElement('div');
-  $player2.classList.add('player2');
+// function createPlayer2() {
+//   const $player = document.createElement('div');
+//   $player.classList.add('player2');
 
-  const $progressbar = document.createElement('div');
-  $progressbar.classList.add('progressbar');
-  $player2.appendChild($progressbar);
+//   const $progressbar = document.createElement('div');
+//   $progressbar.classList.add('progressbar');
+//   $player.appendChild($progressbar);
 
-  const $character = document.createElement('div');
-  $character.classList.add('character');
-  $player2.appendChild($character);
+//   const $character = document.createElement('div');
+//   $character.classList.add('character');
+//   $player.appendChild($character);
 
-  const $life = document.createElement('div');
-  $life.classList.add('life');
-  $life.style.width = '100%';
-  // $life.innerText = life;
-  $progressbar.appendChild($life);
+//   const $life = document.createElement('div');
+//   $life.classList.add('life');
+//   $life.style.width = '100%';
+//   // $life.innerText = life;
+//   $progressbar.appendChild($life);
 
-  const $name = document.createElement('div');
-  $name.classList.add('name');
-  $name.innerText = 'Kitana';
-  $progressbar.appendChild($name);
+//   const $name = document.createElement('div');
+//   $name.classList.add('name');
+//   $name.innerText = 'Kitana';
+//   $progressbar.appendChild($name);
 
-  const $img = document.createElement('img');
-  $img.classList.add('img');
-  $img.src = 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif';
-  $character.appendChild($img);
+//   const $img = document.createElement('img');
+//   $img.classList.add('img');
+//   $img.src = 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif';
+//   $character.appendChild($img);
 
-  const $arenas = document.querySelector('.arenas');
-  $arenas.appendChild($player2);
-};
+//   const $arenas = document.querySelector('.arenas');
+//   $arenas.appendChild($player);
+// };
 
 // createPlayer1();
 // createPlayer2();
@@ -88,5 +88,12 @@ function createPlayer2() {
 // createPlayer1('player1', 'Subzero', 73);
 // createPlayer2('player2', 'Kitana', 54);
 
-createPlayer1('player1', subzero);
+// createPlayer1('player1', subzero);
+
+// createPlayer('player1');
+// createPlayer('player2');
+
+createPlayer('player1', 'subzero', 50, subzero);
+createPlayer('player2', 'kitana', 78, kitana);
+
 
